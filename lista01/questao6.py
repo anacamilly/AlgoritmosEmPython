c6fb575823c390894720a773ef18698e4395f3cc
Ana Camilly <camillyguedes2@gmail.com>
2022-06-01 19:56:16 -0300

    adiciona questao6 da lista1

---
+++ lista01/questao6.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número: "))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("O antecessor dele é: " + str(antecessor))
+print("O sucessor dele é: " + str(sucessor))
