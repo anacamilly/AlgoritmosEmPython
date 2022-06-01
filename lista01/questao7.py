bc2e138f7fdbe8e664480f054eaa6cf1800f5e2f
Ana Camilly <camillyguedes2@gmail.com>
2022-06-01 19:58:18 -0300

    adiciona questao7 da lista1

---
+++ lista01/questao7.py
@@ -0,0 +1,4 @@
+temperaturaEmC = float(input("Digite a temperatura em graus Celsius: "))
+
+temperaturaEmF = 9/5 * temperaturaEmC + 32
+print("A temperatura em graus Farenheit é: " + str(temperaturaEmF) + "F")
